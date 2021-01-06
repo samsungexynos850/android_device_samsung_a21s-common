8f878b8ef2da7aa9dcbbc1be05f07fa84145800d
LuK1337 <priv.luk@gmail.com>
2021-01-06 19:30:19 +0100

    a21s-common: Build vndservicemanager
    * Needed for FCM >= R.
    
    Change-Id: Ibc27a98f5774a51caaab077c1d8e0a2a79bdffe5

--- common.mk
+++ common.mk
@@ -241,6 +241,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.samsung
 
+# Vendor service manager
+PRODUCT_PACKAGES += \
+    vndservicemanager
+
 # WiFi
 PRODUCT_PACKAGES += \
     WifiOverlay \
