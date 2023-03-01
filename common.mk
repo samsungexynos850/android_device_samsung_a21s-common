eafa7b5b427efb4947aa385ca18c086e74f62ba8
Luofan Chen <clfbbn@gmail.com>
2023-03-01 20:51:49 +0800

    a21s-common: Build lineage health HAL
    Change-Id: I1ca5f25f3a2d8734647b9cc0ff4885b0b817c6ad

--- common.mk
+++ common.mk
@@ -158,6 +158,10 @@ PRODUCT_PACKAGES += \
 
 $(call soong_config_set,samsungVars,target_keymaster4_library,//vendor/samsung/a21s-common:libskeymaster4device)
 
+# Lineage Health
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # Media
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml
