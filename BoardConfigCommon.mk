8e62a3b751d17cb56a206498e0b756ea5d558647
RiskyGUY22 <thomas_turner@talktalk.net>
2024-04-04 14:04:18 +0100

    I dont even know at this point
    Signed-off-by: RiskyGUY22 <thomas_turner@talktalk.net>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -23,17 +23,18 @@ include vendor/samsung/a21s-common/BoardConfigVendor.mk
 
 ## Architecture
 TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-a
+TARGET_ARCH_VARIANT := armv8-2a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT := cortex-a55
+TARGET_CPU_SMP := true
 
 ## Architecture (Secondary)
 TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_ARCH_VARIANT := armv8-2a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
-TARGET_2ND_CPU_VARIANT := cortex-a15
+TARGET_2ND_CPU_VARIANT := cortex-a55
 
 ## AUDIO
 USE_XML_AUDIO_POLICY_CONF := 1
@@ -155,7 +156,7 @@ BOARD_ROOT_EXTRA_FOLDERS := \
 BOARD_VENDOR := samsung
 TARGET_BOARD_PLATFORM := universal3830
 TARGET_BOOTLOADER_BOARD_NAME := exynos850
-TARGET_SOC := exynos850
+TARGET_SOC := exynos3830
 include hardware/samsung_slsi-linaro/config/BoardConfig850.mk
 
 ## Properties
--- common.mk
+++ common.mk
@@ -131,14 +131,14 @@ PRODUCT_PACKAGES += \
 # init
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/init/fstab.exynos850:$(TARGET_COPY_OUT_RAMDISK)/fstab.exynos850 \
-    $(COMMON_PATH)/configs/init/fstab.sqzr:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.sqzr \
+    $(COMMON_PATH)/configs/init/fstab.ramplus:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.ramplus \
     $(COMMON_PATH)/configs/init/fstab.exynos850:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.exynos850 \
     $(COMMON_PATH)/configs/init/init.exynos850.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.exynos850.rc \
     $(COMMON_PATH)/configs/init/init.exynos850.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.exynos850.usb.rc \
     $(COMMON_PATH)/configs/init/init.recovery.exynos850.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.exynos850.rc \
     $(COMMON_PATH)/configs/init/init.samsung.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.samsung.rc \
-    $(COMMON_PATH)/configs/init/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc \
-    $(COMMON_PATH)/configs/init/init.fingerprint.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.fingerprint.rc
+    $(COMMON_PATH)/configs/init/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/etc/ueventd.rc
+
 # Kernel
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
