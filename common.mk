529e9f2119b83a96aef32ee89e1d77d46a35be86
RiskyGUY22 <thomas_turner@talktalk.net>
2024-05-10 20:48:18 +0100

    Update thermal HAL

--- common.mk
+++ common.mk
@@ -281,8 +281,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal-service.samsung \
-    thermal_symlinks.samsung
+	android.hardware.thermal@2.0.vendor
 
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/thermal/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
