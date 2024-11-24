540a726d52285d40a5634be10c58297a950648c9
Thomas Turner <thomas_turner@talktalk.net>
2024-11-24 10:59:11 +0000

    a21s-common: Build Samsung Doze

--- common.mk
+++ common.mk
@@ -35,8 +35,13 @@ PRODUCT_PACKAGES += \
     libtinyalsa \
     libtinycompress
 
+# Dolby
 PRODUCT_PACKAGES += \
     SamsungDAP
+
+# Doze
+PRODUCT_PACKAGES += \
+    SamsungDoze
 
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/a2dp_in_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/a2dp_in_audio_policy_configuration.xml \
