43662094b7ac2d8b4ffb7e4627364494ed5afe69
Tim Zimmermann <tim@linux4.de>
2024-12-06 17:08:36 +0000

    a21s-common: Switch to AIDL usb HAL

--- common.mk
+++ common.mk
@@ -263,7 +263,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service.samsung
+    android.hardware.usb-service.samsung
 
 # Vendor service manager
 PRODUCT_PACKAGES += \
