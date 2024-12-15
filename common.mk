a44446ff09caeebc75d2f756fa24de24e5315c03
Thomas Turner <thomas_turner@talktalk.net>
2024-12-15 13:27:52 +0000

    a21s-common: Add shim for updated libexynosgraphicbuffer

--- common.mk
+++ common.mk
@@ -91,7 +91,8 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl:64 \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.2-service \
-    android.hardware.graphics.mapper@2.0-impl-2.1
+    android.hardware.graphics.mapper@2.0-impl-2.1 \
+    libshim_ui
 
 PRODUCT_PACKAGES += \
     libdrm.vendor:64
