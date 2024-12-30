ab837df279bcbb23e4f5f530db72fd7474712af3
Thomas Turner <thomas_turner@talktalk.net>
2024-12-30 14:00:58 +0000

    a21s-common: Build shim for updated OMX
    * As of Android 14 our device needs this shim for
    * OMX to work, without this all hardware accelerated
    * codecs fail to load, due to a missing symbol.

--- shims/libshim_ui/GraphicBufferMapper.cpp
+++ shims/libshim_ui/GraphicBufferMapper.cpp
@@ -3,12 +3,16 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
+
 #include <stdint.h>
+#include <sync/sync.h>
 #include <ui/GraphicBufferMapper.h>
 #include <ui/Rect.h>
 #include <utils/Errors.h>
+
 using android::Rect;
 using android::status_t;
+
 extern "C" {
 status_t _ZN7android19GraphicBufferMapper9lockAsyncEPK13native_handlemmRKNS_4RectEPPviPiS9_(
         void* thisptr, buffer_handle_t handle, uint64_t producerUsage, uint64_t consumerUsage,
@@ -17,4 +21,25 @@ status_t _ZN7android19GraphicBufferMapper9lockAsyncEPK13native_handlemmRKNS_4Rec
     auto* gpm = static_cast<android::GraphicBufferMapper*>(thisptr);
     return gpm->lockAsync(handle, producerUsage, consumerUsage, bounds, vaddr, fenceFd);
 }
+
+status_t _ZN7android19GraphicBufferMapper9lockAsyncEPK13native_handleyyRKNS_4RectEPPviPiS9_(
+        void* thisptr, buffer_handle_t handle, uint64_t producerUsage, uint64_t consumerUsage,
+        const Rect& bounds, void** vaddr, int fenceFd, int32_t* /*outBytesPerPixel*/,
+        int32_t* /*outBytesPerStride*/) {
+    auto* gpm = static_cast<android::GraphicBufferMapper*>(thisptr);
+    return gpm->lockAsync(handle, producerUsage, consumerUsage, bounds, vaddr, fenceFd);
+}
+
+status_t _ZN7android19GraphicBufferMapper6unlockEPK13native_handle(void* thisptr,
+                                                                   buffer_handle_t handle) {
+    android::base::unique_fd outFence;
+    auto* gpm = static_cast<android::GraphicBufferMapper*>(thisptr);
+    status_t status = gpm->unlock(handle, &outFence);
+    if (status == android::OK && outFence.get() >= 0) {
+        sync_wait(outFence.get(), -1);
+        outFence.reset();
+    }
+    return status;
+}
+
 }
