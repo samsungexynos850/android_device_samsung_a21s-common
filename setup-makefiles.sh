34bed843cf276bcbc61f86a45d7e3b73b6da8981
Thomas Turner <thomas_turner@talktalk.net>
2025-01-09 21:11:53 +0000

    a21s-common: Update Copyright date to 2025

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2024 The LineageOS Project
+# Copyright (C) 2017-2025 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
