bf0b1da5dbb5512ed33b6d791d96757a071008c5
Tim Zimmermann <tim@linux4.de>
2025-03-09 20:14:07 +0000

    a21s-common: Use pixel thermal HAL directly
    Also revert old changes where the vendor thermal HAL
    was used.
    
    Change-Id: I30e0f13acc5f2ae4d46b4d449c49b925b930c4ca

--- common.mk
+++ common.mk
@@ -306,9 +306,11 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0.vendor \
-    android.frameworks.cameraservice.service@2.1.vendor \
-    libnetd_resolv
+    android.hardware.thermal-service.pixel \
+    thermal_symlinks
+
+PRODUCT_COPY_FILES += \
+    $(COMMON_PATH)/configs/thermal/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
 
 # Touch HAL
 PRODUCT_PACKAGES += \
