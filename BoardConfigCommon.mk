ac957ba821509f768448c1b1c02a967b4f8cf2e4
Thomas Turner <thomas_turner@talktalk.net>
2025-03-16 20:00:11 +0000

    a21s-common: Remove device_framework_matrix.xml
    We can now just use the one in hardware/samsung
    ref: https://review.lineageos.org/c/LineageOS/android_hardware_samsung/+/413262

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -111,8 +111,7 @@ AB_OTA_UPDATER := false
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml \
-    $(COMMON_PATH)/device_framework_matrix.xml
+    vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 
