e2cbdf919d9a5853fe4d908b3c02e94a1f312377
Thomas Turner <thomas_turner@talktalk.net>
2025-04-28 16:58:53 +0100

    a21s-common: Remove init.samsung.rc from configs
    This init file contains only sec related stuff where the permissions
    are already defined elsewhere where the HALs are, and everything works
    fine without this file so there is no reason to keep it.

--- common.mk
+++ common.mk
@@ -146,7 +146,6 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/init/init.exynos850.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.exynos850.rc \
     $(COMMON_PATH)/configs/init/init.exynos850.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.exynos850.usb.rc \
     $(COMMON_PATH)/configs/init/init.recovery.exynos850.rc:$(TARGET_COPY_OUT_RECOVERY)/root/init.recovery.exynos850.rc \
-    $(COMMON_PATH)/configs/init/init.samsung.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.samsung.rc \
     $(COMMON_PATH)/configs/init/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/etc/ueventd.rc
 
 # Kernel
