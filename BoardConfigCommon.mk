7697d6cda7a7f0389cb978103e4c826cef050d93
Bruno Martins <bgcngm@gmail.com>
2025-05-12 16:20:03 +0100

    a21s-common: Migrate Lineage Health to soong_config_set
    Change-Id: I3368311fcef8156447bde0d4beaa49b1e70bde98

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -114,12 +114,12 @@ TARGET_INIT_VENDOR_LIB := //$(COMMON_PATH):libinit_exynos3830
 AB_OTA_UPDATER := false
 
 ## Lineage Health
-TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH := /sys/class/power_supply/battery/batt_slate_mode
-TARGET_HEALTH_CHARGING_CONTROL_CHARGING_ENABLED := 0
-TARGET_HEALTH_CHARGING_CONTROL_CHARGING_DISABLED := 1
-TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS := false
-TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_TOGGLE := true
-TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_DEADLINE := false
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/batt_slate_mode)
+$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
+$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
+$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
+$(call soong_config_set,lineage_health,charging_control_supports_toggle,true)
+$(call soong_config_set,lineage_health,charging_control_supports_deadline,false)
 
 ## Manifest
 # HIDL
