39fc7e15a95f0a63a1058264b40f71617b887157
Thomas Turner <thomas_turner@talktalk.net>
2025-06-08 12:35:50 +0100

    a21s-common: Drop AIDL lineage touch HAL
    Our device doesn't actually support it, and even
    when we were building it, there were no interfaces
    being used.

--- common.mk
+++ common.mk
@@ -283,10 +283,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/thermal/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
 
-# Touch HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.samsung
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb-service.samsung \
