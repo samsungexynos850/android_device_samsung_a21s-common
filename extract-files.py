ada3a89e767f5e4744915b06c523aaa8f96dbc2f
Simon1511 <me@simon1511.de>
2025-06-24 10:25:18 +0100

    a21s-common: libsec-ril: Always return an empty value for GET_SMSC_ADDRESS
    * Make parameter of type RilData (second last one) passed to SecRil::RequestComplete()
      in OnGetSmscAddressDone() be NULL, so that the framework passes an empty value when sending SMS
    * Before U this value was not being used, on U the value returned is "SMSC",format_specifier
      which is considered invalid when passed back to RIL for sending SMS, however when an empty value
      is passed RIL will select the default SMSC, like it did on T and sending SMS works again
    
    Change-Id: I608be406969d7fa58f15ef36340aff4ad1ce9fd0
    Co-authored-by: Tim Zimmermann <tim@linux4.de>

--- extract-files.py
+++ extract-files.py
@@ -72,6 +72,9 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libaudioroute.so','libaudioroute_a21s.so'),
     'vendor/lib/libaudioproxy.so': blob_fixup()
         .add_needed('libshim_audioproxy.so'),
+    'vendor/lib64/libsec-ril.so': blob_fixup()
+        .sig_replace('80 0E 40 F9 E1 03 16 AA 82 0C 80 52 E3 03 15 AA',
+            '80 0E 40 F9 E1 03 16 AA 82 0C 80 52 08 00 80 D2'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
